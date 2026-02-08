4c54da5fd516dde2e80c9b67b01baf02cd48ca9f
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-08 22:19:17 +0600

    \`feat: Add Prisma schema and client generation\`

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const port = process.env.PORT || 3000;
 
 async function server() {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+    try {
+        await prisma.$connect();
+        console.log("Connected to the db successfully");
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
 }
 
 server();
