8f7c71fe73763e826e33057ef3a4735ba5a3e780
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 22:03:45 +0600

    Remove unused createEquipment function

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import { equipmentService } from "./equipment.service";
 
 const createEquipment: RequestHandler = async (req, res) => {
-    const equipment = await equipmentService.createEquipment();
+    try
 };
 
 export const equipmentController = {
