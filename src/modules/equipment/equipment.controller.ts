19b4620d3ed241f3203bc55d10f25fcff28c1999
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 22:09:45 +0600

    Update equipment controller to use Prisma for database operations

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -1,13 +1,19 @@
 import { RequestHandler } from "express";
 import { equipmentService } from "./equipment.service";
+import { prisma } from "../../lib/prisma";
 
 const createEquipment: RequestHandler = async (req, res) => {
-    try {
-      const payload = req.body;
-      const equipment = await equipmentService.createEquipment(payload);
-    } catch (error) {
-      
-    }
+  try {
+    const payload = req.body;
+
+    const equipment = await prisma.equipment.create({
+      data: payload,
+    });
+
+    res.send({ message: "Equipment Added", data: equipment });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const equipmentController = {
