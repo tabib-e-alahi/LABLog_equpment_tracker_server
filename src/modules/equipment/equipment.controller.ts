15fcb5fe8c3f1dd7c955cce100e66edf42f116f7
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 22:05:45 +0600

    Implement equipment creation with error handling

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -2,7 +2,12 @@ import { RequestHandler } from "express";
 import { equipmentService } from "./equipment.service";
 
 const createEquipment: RequestHandler = async (req, res) => {
-    try
+    try {
+      const payload = req.body;
+      const equipment = await equipmentService.createEquipment(payload);
+    } catch (error) {
+      
+    }
 };
 
 export const equipmentController = {
--- src/modules/equipment/equipment.service.ts
+++ src/modules/equipment/equipment.service.ts
@@ -1,5 +1,12 @@
-const createEquipment = async () => {
-    console.log("equipment services");
+import { Equipment } from "../../generated/prisma/client";
+import { prisma } from "../../lib/prisma";
+
+const createEquipment = async (payload: Equipment) => {
+    const equipment = await prisma.equipment.create({
+      data:{
+            ...payload
+      }
+    })
 };
 
 export const equipmentService = {
