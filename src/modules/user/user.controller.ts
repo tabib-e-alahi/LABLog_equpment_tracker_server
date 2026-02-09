eb762aa9590fb5f7f34d61f88cfa7edefad58b49
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 09:47:42 +0600

    Remove type parameter from userService.register call.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,6 +4,5 @@ import { userService } from "./user.service";
 
 const register: RequestHandler = async(req, res) =>{
       const payload = req.body;
-
-      const user = await userService.register(payload: User)
+      const user = await userService.register(payload)
 }
