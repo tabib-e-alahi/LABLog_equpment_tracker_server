b17c30f2acb5a3ba3ea1b5e877aff5106e452c36
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 22:15:44 +0600

    Add equipment CRUD API endpoints

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -3,19 +3,29 @@ import { RequestHandler } from "express";
 import { prisma } from "../../lib/prisma";
 
 const createEquipment: RequestHandler = async (req, res) => {
-  try {
-    const payload = req.body;
+    try {
+        const payload = req.body;
 
-    const equipment = await prisma.equipment.create({
-      data: payload,
-    });
+        const equipment = await prisma.equipment.create({
+            data: payload,
+        });
 
-    res.send({ message: "Equipment Added", data: equipment });
-  } catch (error) {
-    console.error(error);
-  }
+        res.send({ message: "Equipment Added", data: equipment });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getEquipments: RequestHandler = async (req, res) => {
+    try {
+        const data = await prisma.equipment.findMany();
+        res.send({ message: "Equipments", data });
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export const equipmentController = {
     createEquipment,
+    getEquipments,
 };
--- src/modules/equipment/equipment.router.ts
+++ src/modules/equipment/equipment.router.ts
@@ -4,5 +4,6 @@ import { equipmentController } from "./equipment.controller";
 const equipmentRouter = Router();
 
 equipmentRouter.post("/", equipmentController.createEquipment);
+equipmentRouter.get("/", equipmentController.getEquipments);
 
 export default equipmentRouter;
