c9a3beeacaa4c2f951c76827df4148a7d9054a0b
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 09:55:42 +0600

    Implement user registration feature with password hashing

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,11 @@
 import { RequestHandler } from "express";
 import { userService } from "./user.service";
 
+const register: RequestHandler = async (req, res) => {
+    try {
+        const payload = req.body;
+        const user = await userService.register(payload);
 
-const register: RequestHandler = async(req, res) =>{
-      const payload = req.body;
-      const user = await userService.register(payload)
-}
+        return
+    } catch (error) {}
+};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,22 +1,18 @@
 import { User } from "../../generated/prisma/client";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { prisma } from "../../lib/prisma";
 
 const register = async (payload: User) => {
-    try {
-      const hashedPass = await bcrypt.hash(payload.password, 10);
+    const hashedPass = await bcrypt.hash(payload.password, 10);
 
-      const user = await prisma.user.create({
-            data: {
-                  ...payload, password: hashedPass
-            }
-      })
+    const user = await prisma.user.create({
+        data: {
+            ...payload,
+            password: hashedPass,
+        },
+    });
 
-      return user;
-
-    } catch (error) {
-      
-    }
+    return user;
 };
 
 export const userService = {
