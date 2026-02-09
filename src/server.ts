a1a10092f549d57670ade5e03b0ab822c256361b
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 21:09:44 +0600

    Update server port to 5000

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import app from "./app";
 import { prisma } from "./lib/prisma";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 async function server() {
     try {
         await prisma.$connect();
-        console.log("Connected to the db successfully");
+
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
