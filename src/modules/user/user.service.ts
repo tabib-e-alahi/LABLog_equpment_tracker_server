c2b26ca829511ff77ea951e12ae29efd38643406
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 09:49:41 +0600

    Implement user registration functionality in user service.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,10 @@
-const register = async () => {};
+import { User } from "../../generated/prisma/client";
+
+const register = async (payload: User) => {
+    try {
+      const 
+    } catch (error) {}
+};
 
 export const userService = {
     register,
