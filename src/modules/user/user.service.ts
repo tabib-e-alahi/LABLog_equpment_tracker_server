2ba0678be0fb38674433b46d6675ea9f86751b14
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-09 09:53:42 +0600

    Add user registration to user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,22 @@
 import { User } from "../../generated/prisma/client";
 import bcrypt from "bcrypt"
+import { prisma } from "../../lib/prisma";
 
 const register = async (payload: User) => {
     try {
       const hashedPass = await bcrypt.hash(payload.password, 10);
-    } catch (error) {}
+
+      const user = await prisma.user.create({
+            data: {
+                  ...payload, password: hashedPass
+            }
+      })
+
+      return user;
+
+    } catch (error) {
+      
+    }
 };
 
 export const userService = {
