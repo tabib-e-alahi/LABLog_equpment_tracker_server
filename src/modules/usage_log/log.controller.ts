d2e37974963e0a2e697aaea21e1afae83fbab2e4
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-10 09:31:47 +0600

    Update log controller to create usage logs

--- src/modules/usage_log/log.controller.ts
+++ src/modules/usage_log/log.controller.ts
@@ -1,19 +1,30 @@
 import { RequestHandler } from "express";
 import { prisma } from "../../lib/prisma";
 
-const createLog: RequestHandler = async (req, res) => {
+const createUsageLog: RequestHandler = async (req, res) => {
     try {
         const payload = req.body;
 
-        const data = await prisma.usageLog.create({
+        const log = await prisma.usageLog.create({
             data: payload,
         });
-        res.status(200).json({ message: "usage added", data });
+        res.status(200).json({ message: "usage added", data: log });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const getUsageLog: RequestHandler = async (req, res) => {
+    try {
+        const log = await prisma.usageLog.findMany({
+            data: payload,
+        });
+        res.status(200).json({ message: "usage added", data: log });
     } catch (error) {
         console.error(error);
     }
 };
 
 export const logController = {
-    createLog,
+    createUsageLog,
 };
