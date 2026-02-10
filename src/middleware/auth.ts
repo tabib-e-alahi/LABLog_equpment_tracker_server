a725a5bf074513c3a24bf5a94099cb577e282682
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-10 18:53:49 +0600

    \`Add jsonwebtoken and dependencies\`

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const auth = (roles?: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             const token = req.headers.authorization?.split(" ")[1];
-            if (!token) throw new Error("Un");
-            
+            if (!token) throw new Error("Unauthorized Access!");
+
+            const decode = jwt
         } catch (error) {
             console.error(error);
         }
