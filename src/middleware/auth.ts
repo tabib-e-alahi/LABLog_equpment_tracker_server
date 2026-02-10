e382a6b054a037b9479252f9423bf9c488bece07
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-10 18:49:49 +0600

    Refactor auth middleware to use Express request/response types

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,7 @@
+import { Request, Response, NextFunction } from "express"
 
 const auth = (roles?: string[])=>{
-      try {
-            
-      } catch (error) {
-            console.error(error);
+      return (req: Request, res: Response, next: NextFunction) =>{
+            const token = 
       }
 }
