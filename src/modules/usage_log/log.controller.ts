5205fea19fda6e7ce43981cdc70e1445bb92dd8e
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-10 09:29:47 +0600

    Implement usage log creation with Prisma

--- src/modules/usage_log/log.controller.ts
+++ src/modules/usage_log/log.controller.ts
@@ -1,11 +1,19 @@
 import { RequestHandler } from "express";
+import { prisma } from "../../lib/prisma";
 
-const createLog: RequestHandler = async(req, res) =>{
-      try {
-            const payload = req.body;
+const createLog: RequestHandler = async (req, res) => {
+    try {
+        const payload = req.body;
 
-            const data = await pris
-            
-      } catch (error) {
-            
-      }
+        const data = await prisma.usageLog.create({
+            data: payload,
+        });
+        res.status(200).json({ message: "usage added", data });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const logController = {
+    createLog,
+};
