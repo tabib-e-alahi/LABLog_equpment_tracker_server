3be7dbfb5e067379984ca8b494e5f77f999cd8fe
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-10 09:49:47 +0600

    Add usage log router to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import userRouter from "../modules/user/user.router";
 import equipmentRouter from "../modules/equipment/equipment.router";
+import usageLogRouter from "../modules/usage_log/log.router";
 
 const routes = Router();
 
 routes.use("/user", userRouter);
 
 routes.use("/equipment", equipmentRouter);
+
+routes.use("/usagelogs", usageLogRouter);
 
 export default routes;
