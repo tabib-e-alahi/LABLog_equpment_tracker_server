530730849d3baaa931937f402b499c6a8dc6e0fa
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-10 18:51:49 +0600

    Update auth middleware to validate token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,13 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction } from "express";
 
-const auth = (roles?: string[])=>{
-      return (req: Request, res: Response, next: NextFunction) =>{
-            const token = 
-      }
-}
+const auth = (roles?: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const token = req.headers.authorization?.split(" ")[1];
+            if (!token) throw new Error("Un");
+            
+        } catch (error) {
+            console.error(error);
+        }
+    };
+};
