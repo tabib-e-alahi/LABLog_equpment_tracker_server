75330a1a356bcd79136580440ee0d2737ea2d20a
tabib-e-alahi <etabib.alahi@gmail.com>
2026-02-11 09:55:52 +0600

    Add Prisma adapter to auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,8 @@
 import { betterAuth } from "better-auth";
-
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
 export const auth = betterAuth({
-  //...
+      database: prismaAdapter(prisma, {
+            
+      })
 });
